a7629546455170c16289df5e20dd23551550c504
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-08-31 18:47:36 +0200

    Using vector comparisons in tests.

--- fccl_control/test/Features.cpp
+++ fccl_control/test/Features.cpp
@@ -32,18 +32,15 @@ TEST_F(FeaturesTest, Point)
   // basic constructor, getter, and setter tests
   Point point(name, pos);
   Point point2;
-  point2.setName("corner");
+  point2.setName(name);
   point2.setPosition(pos);
-  EXPECT_TRUE(KDL::Equal(point.getPosition().getVector(), point2.getPosition().getVector()));
-  EXPECT_STREQ(point.getPosition().getFrameName().c_str(), point2.getPosition().getFrameName().c_str());
+  EXPECT_EQ(point.getPosition(), point2.getPosition());
   EXPECT_STREQ(point.getName().c_str(), point2.getName().c_str());
   EXPECT_STRNE(point.getName().c_str(), "corner2");
   EXPECT_STRNE(point.getName().c_str(), "");
 
   // changing reference frame
   point.changeReferenceFrame(transform);
-  EXPECT_FALSE(KDL::Equal(point.getPosition().getVector(), point2.getPosition().getVector()));
-  EXPECT_TRUE(KDL::Equal(point.getPosition().getVector(), KDL::Vector(0,0,-1)));
-  EXPECT_STREQ(point.getName().c_str(), point2.getName().c_str());
-  EXPECT_STRNE(point.getPosition().getFrameName().c_str(), point2.getPosition().getFrameName().c_str());
+  EXPECT_NE(point.getPosition(), point2.getPosition());
+  EXPECT_EQ(point.getPosition(), Vector(KDL::Vector(0,0,-1), "child"));
 }
