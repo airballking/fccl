a37a278a7994f2e708e355bae737f98c376fcb5d
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-09-05 14:23:39 +0200

    Hashing functionality to utilities package.

---
+++ fccl_utils/include/fccl_utils/Hashing.h
@@ -0,0 +1,12 @@
+#ifndef FCCL_UTILS_HASHING_H
+#define FCCL_UTILS_HASHING_H
+
+#include <string>
+
+namespace fccl
+{
+
+  std::size_t hash(const std::string& in);
+
+} // namespace fccl
+#endif // FCCL_UTILS_HASHING_H
