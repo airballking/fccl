d14d2adaa22acdad26c40dab98fe6de95b365fbc
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-09-09 21:34:14 +0200

    Added ability to retrieve string-values used for hashing.

--- fccl_core/include/fccl/utils/Hashing.h
+++ fccl_core/include/fccl/utils/Hashing.h
@@ -2,14 +2,19 @@
 #define FCCL_UTILS_HASHING_H
 
 #include <string>
+#include <map>
 
 namespace fccl
 {
-namespace utils
-{
+  namespace utils
+  {  
+    static std::map<std::size_t, std::string> hash_memory_;
 
-  std::size_t hash(const std::string& in);
+    std::size_t hash(const std::string& value);
+  
+    void rememberHashValuePair(std::size_t hash, const std::string& value);
 
-} // namespace fccl::utils
+    const std::string& retrieveValue(std::size_t hash);
+  } // namespace fccl::utils
 } // namespace fccl
 #endif // FCCL_UTILS_HASHING_H
--- fccl_core/src/fccl/utils/Hashing.cpp
+++ fccl_core/src/fccl/utils/Hashing.cpp
@@ -1,11 +1,29 @@
 #include <fccl/utils/Hashing.h>
+#include <assert.h>
 
-std::size_t fccl::utils::hash(const std::string& in)
+std::size_t fccl::utils::hash(const std::string& value)
 {
   std::size_t result = 0;
 
-  for(unsigned int i=0; i<in.size(); i++)
-    result += 101*in[i];
+  for(unsigned int i=0; i<value.size(); i++)
+    result += 101*value[i];
+
+  rememberHashValuePair(result, value);
 
   return result;
+}
+
+void fccl::utils::rememberHashValuePair(std::size_t hash, const std::string& value)
+{
+  std::pair<std::map<std::size_t, std::string>::iterator, bool> ret;
+  ret = hash_memory_.insert(std::pair<std::size_t, std::string>(hash, value));
+}
+
+const std::string& fccl::utils::retrieveValue(std::size_t hash)
+{
+  std::map<std::size_t, std::string>::iterator it = hash_memory_.find(hash);
+
+  assert(it != hash_memory_.end());
+
+  return it->second;
 }
