235a00589dad1ab5b2f266768f88b0ebe765063b
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-09-10 19:47:36 +0200

    Started sketching KinematicChain and Robot interface. Far from done, yet.

---
+++ fccl_core/include/fccl/kdl/Robot.h
@@ -0,0 +1,68 @@
+#include <fccl/kdl/Jacobian.h>
+#include <fccl/kdl/Transform.h>
+#include <fccl/kdl/Semantics.h>
+#include <fccl/kdl/JntArray.h>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/tree.hpp>
+#include <boost/shared_ptr.hpp>
+
+typedef boost::shared_ptr<KDL::ChainFkSolverPos_recursive> FkSolverPosPtr;
+typedef boost::shared_ptr<KDL::ChainJntToJacSolver> JacobianSolverPtr;
+
+class KinematicChain : public SemanticObject1x1
+{
+  public:
+    KinematicChain();
+    KinematicChain(const KinematicChain& other);
+    KinematicChain(const fccl::kdl::Chain& chain); 
+
+    virtual ~KinematicChain();
+
+    KinematicChain& operator=(const KinematicChain& other); 
+
+    size_t getReferenceID() const;
+    const std::string& getReferenceName() const;
+
+    size_t getTargetID() const;
+    const std::string& getTargetName() const;
+
+    const kdl::fccl::Chain& getChain() const;
+    void setChain(const fccl::kdl::Chain& chain);
+
+    void calculateJacobian(const fccl::kdl::JntArray& joint_state, 
+        fccl::kdl::Jacobian& jacobian);
+    const fccl::kdl::Jacobian& calculateJacobian(const fccl::kdl::JntArray&
+        joint_state);
+
+    void calculateForwardKinematics(const fccl::kdl::JntArray& joint_state, 
+        fccl::kdl::Transform& transform);
+    const fccl::kdl::Transform& calculateForwardKinematics(
+        const fccl::kdl::JntArray& joint_state);
+
+    virtual bool operator==(const KinematicChain& other) const;
+    virtual bool operator!=(const KinematicChain& other) const;
+
+  private:
+    fccl::kdl::Chain chain_;
+    
+    FkSolverPtr jnt_to_pose_solver_;
+    
+    JacobianSolverPtr jnt_to_jac_solver_;
+
+    fccl::kdl::Jacobian jacobian_;
+
+    fccl::kdl::Transform transform_;
+};
+
+class Robot
+{
+  public:
+    Robot();
+
+  private:
+    KDL::Tree kinematic_tree_;
+
+    
+};
