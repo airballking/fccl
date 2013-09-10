20a02bdbac56ff487bba07b0e7b44413fad41059
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-09-10 02:57:40 +0200

    Refactored Twist to inherit its semantics.

--- fccl_core/include/fccl/kdl/Twist.h
+++ fccl_core/include/fccl/kdl/Twist.h
@@ -5,13 +5,13 @@
 #include <string>
 #include <iostream>
 #include <fccl/kdl/Transform.h>
-#include <Eigen/Core>
+#include <fccl/kdl/Semantics.h>
 
 namespace fccl
 {
   namespace kdl
   {
-    class Twist
+    class Twist : public SemanticObject1x1
     {
       public:
         Twist();
@@ -25,21 +25,12 @@ namespace fccl
   
         fccl::kdl::Twist& operator=(const fccl::kdl::Twist& rhs);
   
-        std::size_t getReferenceID() const;
-        void setReferenceID(std::size_t reference_id);
-        void setReferenceName(const std::string& reference_name);
-  
-        std::size_t getTargetID() const;
-        void setTargetID(std::size_t target_id);
-        void setTargetName(const std::string& target_name);
-  
         const KDL::Twist& getTwist() const;
         void setTwist(const KDL::Twist& twist);
   
         bool operator==(const fccl::kdl::Twist& other) const;
         bool operator!=(const fccl::kdl::Twist& other) const;
   
-        bool semanticsEqual(const fccl::kdl::Twist& other) const;
         bool numericsEqual(const fccl::kdl::Twist& other) const;
   
         void changeReferenceFrame(const fccl::kdl::Transform& transform);
@@ -52,12 +43,6 @@ namespace fccl
             const fccl::kdl::Twist& twist);
    
       private:
-        // reference frame w.r.t. the twist is defined
-        std::size_t reference_id_;
-  
-        // entity/frame of which this is the twist
-        std::size_t target_id_;
-  
         // numeric representation of the derivative 
         KDL::Twist twist_;
     };
--- fccl_core/src/fccl/kdl/Twist.cpp
+++ fccl_core/src/fccl/kdl/Twist.cpp
@@ -1,33 +1,29 @@
 #include <fccl/kdl/Twist.h>
-#include <fccl/utils/Hashing.h>
 #include <fccl/utils/Printing.h>
-
-namespace fu = fccl::utils;
 
 namespace fccl
 {
   namespace kdl
   {
-    Twist::Twist()
+    Twist::Twist() :
+        SemanticObject1x1()
     {
     }
   
     Twist::Twist(const fccl::kdl::Twist& other) :
-        reference_id_(other.getReferenceID()), target_id_(other.getTargetID()),
-        twist_(other.getTwist())
+        SemanticObject1x1(other), twist_(other.getTwist())
     {
     }
   
     Twist::Twist(const std::string& reference_name, const std::string&
         target_name, const KDL::Twist& twist) :
-        reference_id_(fu::hash(reference_name)), target_id_(fu::hash(target_name)),
-        twist_(twist)
+        SemanticObject1x1(reference_name, target_name), twist_(twist)
     {
     }
   
     Twist::Twist(std::size_t reference_id, std::size_t target_id,
         const KDL::Twist& twist) :
-        reference_id_(reference_id), target_id_(target_id), twist_(twist)
+        SemanticObject1x1(reference_id, target_id), twist_(twist)
     {
     }
   
@@ -46,36 +42,6 @@ namespace fccl
       }
     
       return *this;
-    }
-  
-    std::size_t Twist::getReferenceID() const
-    {
-      return reference_id_;
-    }
-  
-    void Twist::setReferenceID(std::size_t reference_id)
-    {
-      reference_id_ = reference_id;
-    }
-  
-    void Twist::setReferenceName(const std::string& reference_name)
-    {
-      reference_id_ = fu::hash(reference_name);
-    }
-  
-    std::size_t Twist::getTargetID() const
-    {
-      return target_id_;
-    }
-  
-    void Twist::setTargetID(std::size_t target_id)
-    {
-      target_id_ = target_id;
-    }
-  
-    void Twist::setTargetName(const std::string& target_name)
-    {
-      target_id_ = fu::hash(target_name);
     }
   
     const KDL::Twist& Twist::getTwist() const
@@ -98,12 +64,6 @@ namespace fccl
       return !(*this == other);
     }
   
-    bool Twist::semanticsEqual(const fccl::kdl::Twist& other) const
-    {
-      return reference_id_ == other.getReferenceID() &&
-          target_id_ == other.getTargetID();
-    }
-  
     bool Twist::numericsEqual(const fccl::kdl::Twist& other) const
     {
       return KDL::Equal(twist_, other.getTwist());
@@ -113,13 +73,13 @@ namespace fccl
     {
       assert(multiplicationPossible(transform));
   
-      twist_ = transform.getTransform() * twist_;
-      reference_id_ = transform.getReferenceID();
+      setTwist(transform.getTransform() * twist_);
+      setReferenceID(transform.getReferenceID());
     }
   
     bool Twist::multiplicationPossible(const fccl::kdl::Transform& transform) const
     {
-      return reference_id_ == transform.getTargetID();
+      return getReferenceID() == transform.getTargetID();
     }
   
     fccl::kdl::Twist operator*(const fccl::kdl::Transform& lhs, const fccl::kdl::Twist& rhs)
