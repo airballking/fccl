cfca2f258eb9d866944489b3650bae781850d44a
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-09-19 22:10:42 +0200

    Added new semantics object for Transforms.

---
+++ fccl_core/include/fccl/semantics/TransformSemantics.h
@@ -0,0 +1,84 @@
+#ifndef FCCL_SEMANTICS_TRANSFORM_SEMANTICS_H
+#define FCCL_SEMANTICS_TRANSFORM_SEMANTICS_H
+
+#include <fccl/semantics/SemanticsBase.h>
+
+namespace fccl
+{
+  namespace semantics
+  {
+    class TransformSemantics
+    {
+      public:
+        const SemanticsBase& reference() const
+        {
+          return reference_;
+        }
+
+        SemanticsBase& reference()
+        {
+          return reference_;
+        }
+
+        const SemanticsBase& target() const
+        {
+          return target_;
+        }
+
+        SemanticsBase& target()
+        {
+          return target_;
+        }
+
+        bool equals(const TransformSemantics& other) const
+        {
+          return reference().equals(other.reference()) &&
+              target().equals(other.target());
+        }
+
+        TransformSemantics inverse() const
+        {
+          TransformSemantics result(*this);
+
+          result.invert();
+
+          return result;
+        }
+
+        void invert()
+        {
+          SemanticsBase tmp(reference());
+          reference() = target();
+          target() = tmp;
+        }
+
+      private:
+        SemanticsBase reference_;
+        SemanticsBase target_;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const TransformSemantics& obj)
+    {
+      os << "reference: " << obj.reference() << "\n";
+      os << "target: " << obj.target();
+      return os;
+    }
+
+    inline bool areMultipliable(const TransformSemantics& lhs, const TransformSemantics& rhs)
+    {
+      return lhs.target().equals(rhs.reference());
+    }
+
+    inline TransformSemantics multiply(const TransformSemantics& lhs, const TransformSemantics& rhs)
+    {
+      assert(areMultipliable(lhs, rhs));
+
+      TransformSemantics result(lhs);
+      result.target() = rhs.target();
+
+      return result;
+    }
+
+  } // namespace semantics
+} // namespace fccl
+#endif // FCCL_SEMANTICS_TRANSFORM_SEMANTICS_H
