e4b44eaa6aa0415daff86ce536810600ed2b5ad5
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-10-24 14:37:12 +0200

    Added: Tests for SolverWeighted.

--- fccl_core/include/fccl/solvers/WeightedSolver.h
+++ fccl_core/include/fccl/solvers/WeightedSolver.h
@@ -18,22 +18,15 @@ namespace fccl
             const fccl::semantics::JntArraySemantics& column_semantics)
         {
           solver_.reinitialise(row_semantics.size(), column_semantics.size());
-          row_semantics_ = row_semantics;
-          column_semantics_ = column_semantics;
+          this->row_semantics_ = row_semantics;
+          this->column_semantics_ = column_semantics;
         }
 
         bool solve(const JointMappingMatrix& A, const JntArray& ydot,
             const JointMappingMatrix& Wq, const JointMappingMatrix& Wy,
             JntArray& qdot)
         {
-          assert(A.semantics().row_joints().equals(row_semantics_));
-          assert(A.semantics().column_joints().equals(column_semantics_));
-          assert(ydot.semantics().equals(row_semantics_));
-          assert(Wq.semantics().row_joints().equals(row_semantics_));
-          assert(Wq.semantics().column_joints().equals(row_semantics_));
-          assert(Wy.semantics().row_joints().equals(column_semantics_));
-          assert(Wy.semantics().column_joints().equals(column_semantics_));
-          assert(qdot.semantics().equals(column_semantics_));
+          assert(inputValid(A, ydot, Wq, Wy, qdot));
 
           solver_.solve(A.numerics(), ydot.numerics().data, Wq.numerics(),
               Wy.numerics(), qdot.numerics().data);
@@ -43,24 +36,38 @@ namespace fccl
     			const JointMappingMatrix& Wq, const JointMappingMatrix& Wy,
     			JntArray& qdot, JointMappingMatrix& A_inv_weighted) 
         {
-          assert(A.semantics().row_joints().equals(row_semantics_));
-          assert(A.semantics().column_joints().equals(column_semantics_));
-          assert(ydot.semantics().equals(row_semantics_));
-          assert(Wq.semantics().row_joints().equals(row_semantics_));
-          assert(Wq.semantics().column_joints().equals(row_semantics_));
-          assert(Wy.semantics().row_joints().equals(column_semantics_));
-          assert(Wy.semantics().column_joints().equals(column_semantics_));
-          assert(qdot.semantics().equals(column_semantics_));
-          assert(A_inv_weighted.semantics().row_joints().equals(column_semantics_));
-          assert(A_inv_weighted.semantics().column_joints().equals(row_semantics_));
-  
+          assert(inputValid(A, ydot, Wq, Wy, qdot, A_inv_weighted));
+ 
           solver_.solve(A.numerics(), ydot.numerics().data, Wq.numerics(),
               Wy.numerics(), qdot.numerics().data, A_inv_weighted.numerics());
         }
 
       private:
         WDLSSolver solver_;
-        fccl::semantics::JntArraySemantics& row_semantics_, column_semantics_;
+        fccl::semantics::JntArraySemantics row_semantics_, column_semantics_;
+
+        bool inputValid(const JointMappingMatrix& A, const JntArray& ydot,
+            const JointMappingMatrix& Wq, const JointMappingMatrix& Wy,
+            const JntArray& qdot) const
+        {
+          return A.semantics().row_joints().equals(row_semantics_) &&
+              A.semantics().column_joints().equals(column_semantics_) &&
+              ydot.semantics().equals(row_semantics_) &&
+              Wq.semantics().row_joints().equals(column_semantics_) &&
+              Wq.semantics().column_joints().equals(column_semantics_) &&
+              Wy.semantics().row_joints().equals(row_semantics_) &&
+              Wy.semantics().column_joints().equals(row_semantics_) &&
+              qdot.semantics().equals(column_semantics_);
+        }
+
+        bool inputValid(const JointMappingMatrix& A, const JntArray& ydot,
+            const JointMappingMatrix& Wq, const JointMappingMatrix& Wy,
+            const JntArray& qdot, const JointMappingMatrix& A_inv_weighted) const
+        {
+          return inputValid(A, ydot, Wq, Wy, qdot) &&
+              A_inv_weighted.semantics().row_joints().equals(column_semantics_) &&
+              A_inv_weighted.semantics().column_joints().equals(row_semantics_);
+        } 
     };
   } // namespace solvers
 } // namespace fccl
--- fccl_core/test/solvers/WeightedSolver.cpp
+++ fccl_core/test/solvers/WeightedSolver.cpp
@@ -1,20 +1,80 @@
 #include <gtest/gtest.h>
 
 #include <fccl/solvers/WeightedSolver.h>
+#include <fccl/utils/Printing.h>
+
+using namespace fccl::kdl;
+using namespace fccl::solvers;
 
 class WeightedSolverTest : public ::testing::Test
 {
   protected:
     virtual void SetUp()
     {
+      joint_names.push_back("joint0");
+      joint_names.push_back("joint1");
+      joint_names.push_back("joint2");
+      joint_names.push_back("joint3");
+      joint_names.push_back("joint4");
+      joint_names.push_back("joint5");
+      joint_names.push_back("joint6");
+
+      constraint_names.push_back("above");
+      constraint_names.push_back("perpendicular");
+
+      qdot.init(joint_names);
+      ydot.init(constraint_names);
+      A.init(constraint_names, joint_names);
+      Wy.init(constraint_names, constraint_names);
+      Wq.init(joint_names, joint_names);
+      A_inv_weighted.init(joint_names, constraint_names);
+
+      using Eigen::operator<<;
+      qdot.numerics().data.setZero();
+      ydot.numerics().data << 1.2, -0.3;
+      A.numerics() << 0.1, -0.2, 0.3, -0.4, 0.5, -0.6, 0.7,
+                      0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6;
+      Wq.numerics().setIdentity();
+      Wy.numerics().setIdentity();
     }
 
     virtual void TearDown()
     {
     }
 
+    std::vector<std::string> constraint_names, joint_names;
+    JntArray qdot, ydot;
+    JointMappingMatrix A, Wy, Wq, A_inv_weighted;
 };
 
 TEST_F(WeightedSolverTest, Basics)
 {
+  SolverWeighted solver;
+  solver.init(ydot.semantics(), qdot.semantics());
+  ASSERT_TRUE(solver.solve(A, ydot, Wq, Wy, qdot));  
+  
+  ASSERT_TRUE(qdot.semantics().equals(Wq.semantics().row_joints()));
+  Eigen::MatrixXd ydot_reconstructed = A.numerics()*qdot.numerics().data;
+  ASSERT_EQ(ydot_reconstructed.rows(), 2);
+  ASSERT_EQ(ydot_reconstructed.cols(), 1);
+  for(std::size_t i=0; i<ydot.size(); i++)
+    EXPECT_NEAR(ydot.numerics()(i), ydot_reconstructed(i,0), 0.0001);
+
+  qdot.numerics().data.setZero();
+  ASSERT_TRUE(solver.solve(A, ydot, Wq, Wy, qdot, A_inv_weighted));  
+
+  ASSERT_TRUE(qdot.semantics().equals(Wq.semantics().row_joints()));
+  ASSERT_TRUE(A_inv_weighted.semantics().row_joints().equals(qdot.semantics()));
+  ASSERT_TRUE(A_inv_weighted.semantics().column_joints().equals(ydot.semantics()));
+  ydot_reconstructed = A.numerics()*qdot.numerics().data;
+  Eigen::MatrixXd qdot_reconstructed = 
+      A_inv_weighted.numerics()*ydot.numerics().data;
+  ASSERT_EQ(qdot_reconstructed.rows(), 7);
+  ASSERT_EQ(qdot_reconstructed.cols(), 1);
+  ASSERT_EQ(ydot_reconstructed.rows(), 2);
+  ASSERT_EQ(ydot_reconstructed.cols(), 1);
+  for(std::size_t i=0; i<ydot.size(); i++)
+    EXPECT_NEAR(ydot.numerics()(i), ydot_reconstructed(i,0), 0.0001);
+  for(std::size_t i=0; i<qdot.size(); i++)
+    EXPECT_NEAR(qdot.numerics()(i), qdot_reconstructed(i,0), 0.0001);
 }
