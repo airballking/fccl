95a022b578dc998d9e65460b2a00a3c6b6cb52d4
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-10-28 10:22:04 +0100

    Added: Empty file for unit-tests for ConstraintController.

---
+++ fccl_core/test/control/ConstraintController.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include <fccl/control/ConstraintController.h>
+
+class ConstraintControllerTest : public ::testing::Test
+{
+  protected:
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+};
+
+TEST_F(ConstraintControllerTest, Basics)
+{
+}
