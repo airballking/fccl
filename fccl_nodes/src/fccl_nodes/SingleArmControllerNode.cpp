4325b9e48820940681044ebcf4e0a17f0d494bd7
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-11-14 16:35:31 +0100

    Moved node creation for SingleArmController into separate cpp-file.

---
+++ fccl_nodes/src/fccl_nodes/SingleArmControllerNode.cpp
@@ -0,0 +1,12 @@
+#include <fccl_nodes/SingleArmController.h>
+
+// The entry to our application...
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "single_arm_controller");
+  ros::NodeHandle n("~");
+  fccl::nodes::SingleArmController controller(n);
+  ros::spin();
+  controller.stop();
+  return 0;
+}
