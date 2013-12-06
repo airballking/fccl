714f7dd600fa4b97fa67130b9c07684b49560aa4
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-12-06 15:12:15 +0100

    Added: New class 'Joint representing a single robotic joint.

---
+++ fccl_core/include/fccl/kdl/Joint.h
@@ -0,0 +1,51 @@
+#ifndef FCCL_KDL_JOINT_H
+#define FCCL_KDL_JOINT_H
+
+#include <fccl/semantics/SemanticsBase.h>
+#include <limits>
+
+namespace fccl
+{
+  namespace kdl
+  {
+    // TODO(Georg): move this to utils
+    bool areEqual(double a, double b)
+    {
+      return fabs(a-b) < std::numeric_limits<double>::epsilon();
+    }
+
+    class Joint
+    {
+      public:
+        double position() const
+        {
+          return position_;
+        }
+
+        double& position()
+        {
+          return position_;
+        }
+
+        const fccl::semantics::SemanticsBase& semantics() const
+        {
+          return semantics_;
+        }
+
+        fccl::semantics::SemanticsBase& semantics()
+        {
+          return semantics_;
+        }
+
+        bool equals(const Joint& other) const
+        {
+          return semantics().equals(other.semantics()) &&
+              areEqual(position(), other.position());
+        }
+      private:
+        double position_;
+        fccl::semantics::SemanticsBase semantics_;
+    };
+  } // namespace kdl
+} // namespace fccl
+#endif // FCCL_KDL_JOINT_H
---
+++ fccl_core/test/kdl/Joint.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+
+#include <fccl/kdl/Joint.h>
+
+using namespace fccl::kdl;
+
+class JointTest : public ::testing::Test
+{
+  protected:
+    virtual void SetUp()
+    {
+      joint_name = "left_elbow_joint";
+      semantics.setName(joint_name);
+      joint_position = 1.2;
+    }
+
+    virtual void TearDown()
+    {
+
+    }
+
+    fccl::semantics::SemanticsBase semantics;
+    std::string joint_name;
+    double joint_position;
+};
+
+TEST_F(JointTest, Basics)
+{
+  Joint jnt;
+  jnt.semantics() = semantics;
+  jnt.position() = joint_position;
+
+  ASSERT_STREQ(jnt.semantics().getName().c_str(), joint_name.c_str());
+  ASSERT_DOUBLE_EQ(jnt.position(), joint_position);
+
+  Joint jnt2(jnt);
+  EXPECT_TRUE(jnt.equals(jnt2));
+  EXPECT_STREQ(jnt2.semantics().getName().c_str(), joint_name.c_str());
+  EXPECT_DOUBLE_EQ(jnt2.position(), joint_position);
+
+  Joint jnt3;
+  jnt3 = jnt;
+  EXPECT_TRUE(jnt.equals(jnt3));
+
+  Joint jnt4;
+  jnt4.semantics() = jnt.semantics();
+  jnt4.position() = jnt.position();
+  EXPECT_TRUE(jnt.equals(jnt4));
+
+  Joint jnt5;
+  EXPECT_FALSE(jnt.equals(jnt5));
+  jnt5.semantics() = semantics;
+  EXPECT_FALSE(jnt.equals(jnt5));
+  jnt5.position() = joint_position;
+  jnt5.semantics().setName("");
+  EXPECT_FALSE(jnt.equals(jnt5));
+}
