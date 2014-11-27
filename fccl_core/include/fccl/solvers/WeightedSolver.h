2bca73da356d4bcafa561616a442f1e264f21860
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-11-27 19:51:03 +0100

    Added a special weighted solver for controlling in Cartesian Twist space.

--- fccl_core/include/fccl/solvers/WeightedSolver.h
+++ fccl_core/include/fccl/solvers/WeightedSolver.h
@@ -4,6 +4,7 @@
 #include <fccl/solvers/WDLSSolver.h>
 #include <fccl/kdl/JntArray.h>
 #include <fccl/kdl/JointMappingMatrix.h>
+#include <fccl/kdl/InteractionMatrix.h>
 
 using namespace fccl::kdl;
 
@@ -20,6 +21,15 @@ namespace fccl
           solver_.reinitialise(row_semantics.size(), column_semantics.size());
           this->row_semantics_ = row_semantics;
           this->column_semantics_ = column_semantics;
+        }
+
+        void init(const fccl::semantics::JntArraySemantics& row_semantics,
+            const fccl::semantics::TwistSemantics& twist_semantics)
+        {
+          solver_.reinitialise(row_semantics.size(), 6);
+          twist_array_.resize(6);
+          this->row_semantics_ = row_semantics;
+          this->twist_semantics_ = twist_semantics;
         }
 
         bool solve(const JointMappingMatrix& A, const JntArray& ydot,
@@ -42,9 +52,25 @@ namespace fccl
               Wy.numerics(), qdot.numerics().data, A_inv_weighted.numerics());
         }
 
+        bool solve(const InteractionMatrix& H, const JntArray& ydot,
+            const JointMappingMatrix& Wt, const JointMappingMatrix& Wy,
+            Twist& twist)
+        {
+          assert(inputValid(H, ydot, Wt, Wy, twist));
+
+          bool result = solver_.solve(H.numerics(), ydot.numerics().data, 
+              Wt.numerics(), Wy.numerics(), twist_array_.numerics().data);
+
+          JntArrayToTwist(twist_array_.numerics(), twist.numerics());
+
+          return result;
+        }
+ 
       private:
         WDLSSolver solver_;
         fccl::semantics::JntArraySemantics row_semantics_, column_semantics_;
+        fccl::semantics::TwistSemantics twist_semantics_;
+        fccl::kdl::JntArray twist_array_;
 
         bool inputValid(const JointMappingMatrix& A, const JntArray& ydot,
             const JointMappingMatrix& Wq, const JointMappingMatrix& Wy,
@@ -68,6 +94,20 @@ namespace fccl
               A_inv_weighted.semantics().row_joints().equals(column_semantics_) &&
               A_inv_weighted.semantics().column_joints().equals(row_semantics_);
         } 
+
+        bool inputValid (const InteractionMatrix& A, const JntArray& ydot,
+            const JointMappingMatrix& Wt, const JointMappingMatrix& Wy,
+            Twist& twist)
+        {
+          return A.semantics().joints().equals(row_semantics_) &&
+              A.semantics().twist().equals(twist_semantics_) &&
+              ydot.semantics().equals(row_semantics_) &&
+              Wt.numerics().rows() == 6 &&
+              Wt.numerics().cols() == 6 &&
+              Wy.semantics().row_joints().equals(row_semantics_) &&
+              Wy.semantics().column_joints().equals(row_semantics_) &&
+              twist.semantics().equals(twist_semantics_);
+        }
     };
   } // namespace solvers
 } // namespace fccl
--- fccl_core/test/solvers/WeightedSolver.cpp
+++ fccl_core/test/solvers/WeightedSolver.cpp
@@ -78,3 +78,60 @@ TEST_F(WeightedSolverTest, Basics)
   for(std::size_t i=0; i<qdot.size(); i++)
     EXPECT_NEAR(qdot.numerics()(i), qdot_reconstructed(i,0), 0.0001);
 }
+
+TEST_F(WeightedSolverTest, CartesianSolving)
+{
+  // variables
+  std::vector<std::string> constraint_names;
+  std::string tool_frame = "tool_frame";
+  std::string world_frame = "world_frame";
+ 
+  Twist twist;
+  JntArray ydot;
+  InteractionMatrix H;
+  JointMappingMatrix Wt, Wy;
+
+  WeightedSolver solver;
+ 
+  // init
+  constraint_names.push_back("above");
+  constraint_names.push_back("perpendicular");
+
+  ydot.init(constraint_names); 
+  twist.semantics().init(world_frame, tool_frame);
+  H.init(constraint_names, world_frame, tool_frame);
+
+  Wt.resize(6, 6);
+  Wy.init(constraint_names, constraint_names);
+
+  using Eigen::operator<<;
+  KDL::SetToZero(twist.numerics());
+  ydot.numerics().data << 1.2, -0.3;
+  H.numerics() << 0.1, -0.2, 0.3, -0.4, 0.5, -0.6,
+                  0.0, 0.1, 0.2, 0.3, 0.4, 0.5;
+  Wy.numerics().setIdentity();
+  Wt.numerics().setIdentity();
+
+  solver.init(ydot.semantics(), twist.semantics());  
+
+  // running calculation
+  solver.solve(H, ydot, Wt, Wy, twist);
+
+  // checking output semantics
+  ASSERT_STREQ(world_frame.c_str(), twist.semantics().reference().getName().c_str());
+  ASSERT_STREQ(tool_frame.c_str(), twist.semantics().target().getName().c_str());
+
+  // running reconstruction
+  KDL::JntArray twist_array(6);
+  TwistToJntArray(twist.numerics(), twist_array);
+  Eigen::MatrixXd ydot_reconstructed = H.numerics()*twist_array.data;
+ 
+  //checking reconstruction
+  EXPECT_EQ(ydot_reconstructed.rows(), ydot.numerics().rows());
+  EXPECT_EQ(ydot_reconstructed.cols(), 1);
+
+  for(unsigned int i=0; i<ydot_reconstructed.rows(); ++i)
+  {
+    EXPECT_NEAR(ydot.numerics()(i), ydot_reconstructed(i,0), 0.001);
+  }
+}
